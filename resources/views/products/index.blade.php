af6a5aaea89f2dbc18ca0dd1faefe3fa03f928f7
Yasuko kawashima <yasuko.kawashima@gmail.com>
2023-08-02 21:04:32 +0900

    16章ユーザ情報を編集エラーあり

--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -3,7 +3,7 @@
  @section('content')
     <div class="row">
         <div class="col-2">
-            @component('components.sidebar' , ['categories' => $categories, 'major_category_names' => $major_category_names])
+            @component('components.sidebar', ['categories' => $categories, 'major_category_names' => $major_category_names])
             @endcomponent
         </div>
      <div class="col-9">
@@ -12,6 +12,11 @@
                 <a href="{{ route('products.index') }}">トップ</a> > <a href="#">{{ $category->major_category_name }}</a> > {{ $category->name }}
                 <h1>{{ $category->name }}の商品一覧{{$total_count}}件</h1>
                 @endif
+            </div>
+            <div>
+                Sort By
+                @sortablelink('id', 'ID')
+                @sortablelink('price', 'Price')
             </div>
          <div class="container mt-4">
              <div class="row w-100">
